72ce0b01d9e3a8b52188b948736a96984743b9d7
senai_25_26 <jhmsg45@gmail.com>
2026-02-02 08:52:41 -0300

    Frontend_3S ope

--- app.py
+++ app.py
@@ -1,54 +1,60 @@
-from flask import Flask, flash
-from flask import render_template, redirect, request, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template("home.html")
 
-@app.route('/calculo')
-def calculo():
-    return render_template('calculo.html')
+@app.route('/calculos')
+def calculos():
+    return render_template("calculos.html")
 
-@app.route('/operaçoes')
+@app.route('/operacoes')
 def operacoes():
-    return render_template('operacoes.html')
+    return render_template("operacoes.html")
 
-@app.route('/soma', methods=['GET','POST'])
-def soma():
+@app.route('/somar', methods=['GET', 'POST'])
+def somar():
     if request.method == 'POST':
-        if not request.form['form_num_1'] or not request.form['form_num_2']:
-            flash("preencha o campo dos numeros", "error")
-        num1 = request.form['form_num_1']
-        num2 = request.form['form_num_2']
-        try:
-            num1 = int(num1)
-            num2 = int(num2)
-            result = num1 + num2
-            print('resultado: ', result)
-            return redirect(url_for('resultado',result=result))
-        except:
-            flash("preencha o campo dos numeros", "error")
-    return render_template('soma.html')
+        if request.form['form-soma_1'] and request.form['form-soma_2']:
+            n1 = int(request.form['form-soma_1'])
+            n2 = int(request.form['form-soma_2'])
+            soma = n1 + n2
+            return render_template("operacoes.html", n1=n1, n2=n2, soma=soma)
+    return render_template("operacoes.html")
 
-@app.route('/subtracao')
-def subtracao():
-    return render_template('subtacao.html')
+@app.route('/subtrarir', methods=['GET', 'POST'])
+def subtrarir():
+    if request.method == 'POST':
+        if request.form['form-sub_1'] and request.form['form-sub_2']:
+            n1 = int(request.form['form-sub_1'])
+            n2 = int(request.form['form-sub_2'])
+            subtracao = n1 - n2
+            return render_template("operacoes.html", n1=n1, n2=n2, subtracao=subtracao)
+    return render_template("operacoes.html")
 
-@app.route('/multiplicacao')
-def multiplicacao():
-    return render_template('multiplicacao.html')
+@app.route('/multiplicar', methods=['GET', 'POST'])
+def multiply():
+    if request.method == 'POST':
+        if request.form['form-mult_1'] and request.form['form-mult_2']:
+            n1 = int(request.form['form-mult_1'])
+            n2 = int(request.form['form-mult_2'])
+            multiplicar = n1 * n2
+            return render_template("operacoes.html", n1=n1, n2=n2, multiplicar=multiplicar)
+    return render_template("operacoes.html")
 
-@app.route('/divisao')
-def divisao():
-    return render_template('divisao.html')
+@app.route('/dividir', methods=['GET', 'POST'])
+def dividir():
+    if request.method == 'POST':
+        if request.form['form-divi_1'] and request.form['form-divi_2']:
+            n1 = int(request.form['form-divi_1'])
+            n2 = int(request.form['form-divi_2'])
+            divisao = n1 / n2
+            return render_template("operacoes.html", n1=n1, n2=n2, divisao=divisao)
+    return render_template("operacoes.html")
 
-
-@app.route('/resultado')
-def resultado():
-    return render_template('resultado.html')
-
+#TODO Final do código
 
 if __name__ == '__main__':
     app.run(debug=True)
